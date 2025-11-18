0c073aa5e03ba7c57e20ef9d3e62d2bf431678b1
Ricardo Cardozo <ricardo.alexandre@innovo.com.br>
2025-11-17 21:17:12 -0300

    correct variable

--- src/app/api/payment/create/route.ts
+++ src/app/api/payment/create/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 import crypto from "crypto";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE!
 );
 
