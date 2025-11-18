d9b5446d8246c28e28da5d124d108dcdccc9e7e8
Ricardo Cardozo <ricardo.alexandre@innovo.com.br>
2025-11-17 21:45:35 -0300

    asd

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://vlbjivrwlxvqywbjweeb.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsYmppdnJ3bHh2cXl3Ymp3ZWViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDAzNzUsImV4cCI6MjA3ODYxNjM3NX0.WpvfEvYAO8jJcy6zYSKlUznrvusJYhtqLiYiSnZs6Wo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
